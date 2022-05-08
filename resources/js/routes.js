b6c88d36cb5b944b5974470dd44edc3f00f48d3a
Navi <mailvoornavi@gmail.com>
2022-05-08 20:42:05 +0200

    added `home` controller

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -8,4 +8,22 @@ export const routes = [
 		name: 'home',
 		component: Container('HomePage')
 	},
+
+	{
+		path: '/login',
+		name: 'login',
+		component: Container('Auth/LoginPage')
+	},
+
+	{
+		path: '/register',
+		name: 'register',
+		component: Container('Auth/RegisterPage')
+	},
+
+	{
+		path: '/user/user-dashboard',
+		name: 'user.user-dashboard',
+		component: Container('Auth/DashboardPage')
+	},
 ]
