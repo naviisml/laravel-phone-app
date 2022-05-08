6b6570ee73f09d06b72d2743dddb56ed118333de
Navi <mailvoornavi@gmail.com>
2022-05-08 20:42:15 +0200

    added `register` controller & endpoint
    added register test

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    use RegistersUsers;
+
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware(['guest', 'guest:api']);
+    }
+
+    /**
+     * The user has been registered.
+     */
+    protected function registered(Request $request, User $user)
+    {
+        return response()->json($user);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'username' => 'required|max:255',
+            'email' => 'required|email:filter|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     */
+    protected function create(array $data): User
+    {
+        $user = User::create([
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+		return $user;
+    }
+}
