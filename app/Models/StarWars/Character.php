7db0b8562039c8431ee356da8759031d9035891d
Navi <mailvoornavi@gmail.com>
2022-05-10 15:18:03 +0200

    created `characters` table & model

---
+++ app/Models/StarWars/Character.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models\StarWars;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Character extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'star_wars_characters';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'height',
+        'mass',
+        'hair_color',
+        'skin_color',
+        'eye_color',
+        'birth_year',
+        'gender',
+        'homeworld',
+        'films',
+        'species',
+        'vehicles',
+        'starships',
+        'url',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'films' => 'array',
+        'species' => 'array',
+        'vehicles' => 'array',
+        'starships' => 'array',
+    ];
+
+}
---
+++ database/migrations/2022_05_10_125047_create_star_wars_characters_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('star_wars_characters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('height')->nullable();
+            $table->string('mass')->nullable();
+            $table->string('hair_color')->nullable();
+            $table->string('skin_color')->nullable();
+            $table->string('eye_color')->nullable();
+            $table->string('birth_year')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('homeworld')->nullable();
+            $table->json('films')->nullable();
+            $table->json('species')->nullable();
+            $table->json('vehicles')->nullable();
+            $table->json('starships')->nullable();
+            $table->string('url')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('star_wars_characters');
+    }
+};
